ce7be6d9e95026745a97c69a8ab2de574bb837d4
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-06-12 23:37:31 +0500

    feat: add Terraform configuration for S3 backend and DynamoDB state locking

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "rs-school.andrey.tf"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "rs-school.andrey.tf_state_lock"
+  }
+}
